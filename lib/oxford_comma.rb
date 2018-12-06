81bbd194bd91a9500c5245085621fbb80c7c9309
Evan Pavley <epavley@sva.edu>
2018-12-06 13:14:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,7 @@
 def oxford_comma(array)
   #if the array has one element in it
-  if array.size == 1 
-    #return the array unchanged
-    return array
     #if the array has 2 elements in it
-    elsif array.size == 2
+  if array.size == 2
     #add an "and" inbetween
     return array.join(" and ")
     #if the array has 3 or more elements in it
@@ -12,5 +9,6 @@ def oxford_comma(array)
     #seperate by commas until the last index, at that index add and
     # AKA access the array at its last index
     array[-1]= "and #{array[-1]}"
-    return array.join(", ")
+  end
+  return array.join(", ")
 end
